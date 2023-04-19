d29317a5e841bc2ba1d9d583f91deb50cc004c4e
thegr <thegr@DESKTOP-J3TFLGM>
2023-04-19 22:40:40 +0800

    first commit

---
+++ src/java/Tables/Incident.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tables;
+import java.sql.*;
+
+/**
+ *
+ * @author thegr
+ */
+public class Incident {
+    
+    public String date;
+    public String descript;
+    public int ID;
+    public int officer;
+    public String penalty;
+    public int rulebook_no;
+    public int seconding_officer;
+    
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : index
+    Created on : Apr 19, 2023, 9:50:43 PM
+    Author     : thegr
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
